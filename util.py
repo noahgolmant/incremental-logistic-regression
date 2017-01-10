6ff53e3d9869b0a9653a4a7b46d185c0ba740fcd
Noah Golmant <noah.golmant@gmail.com>
2017-01-10 13:11:18 -0800

    add timing code and separate plotting notebook

--- util.py
+++ util.py
@@ -2,7 +2,7 @@ import numpy as np
 import csv
 from sklearn.linear_model import LogisticRegression
 from sklearn import metrics
-
+import time
 
 def shuffle(X, y):
     permutation = np.arange(X.shape[0])
@@ -32,21 +32,63 @@ def load_keystone_data(train_filename, test_filename):
     optimal_test = test_records[-1]
     return np.array(train_records[:-1]), optimal_train, np.array(test_records[:-1]), optimal_test
 
+# each batch includes all data up to and including current index.
+def load_optimal_batches(data, labels, num_batches):
+    batch_size = int(data.shape[0] / num_batches)
+    data_batches = []
+    label_batches = []
+    for i in range(num_batches):
+        k = (i + 1) * batch_size
+        data_batches.append(data[:k])
+        label_batches.append(labels[:k])
+    return data_batches, label_batches
 
+# each batch includes only that subset's data.
+def load_incremental_batches(data, labels, num_batches):
+    batch_size = int(data.shape[0] / num_batches)
+    data_batches = []
+    label_batches = []
+    for i in range(num_batches):
+        k = i * batch_size
+        data_batches.append(data[k:k+batch_size])
+        label_batches.append(labels[k:k+batch_size])
+    return data_batches, label_batches
+
+def load_incremental_labels(labels, num_batches):
+    batch_size = int(len(labels) / num_batches)
+    label_batches = []
+    for i in range(num_batches):
+        k = i * batch_size
+        label_batches.append(labels[k:k+batch_size])
+    return label_batches
 
 class Metrics:
-    def __init__(self, train_labels, test_labels):
+    def __init__(self, train_label_batches, test_labels):
         # track metrics for all runs
         self.total_train_loss = []
         self.total_train_error = []
         self.total_test_loss = []
         self.total_test_error = []
         self.test_labels = test_labels
-        self.train_labels = train_labels
+        self.train_label_batches = train_label_batches
+        self.total_train_times = []
 
-    def add_predictions(self, train_predictions, test_predictions):
-        self.train_errors.append(metrics.mean_absolute_error(self.train_labels, train_predictions))
-        self.train_losses.append(metrics.log_loss(self.train_labels, train_predictions))
+    def start_trial(self):
+        self.start_time = time.time()
+    
+    def end_trial(self):
+        end_time = time.time()
+        elapsed_time = end_time - self.start_time
+        if len(self.cumulative_times) == 0:
+            prev_time = 0
+        else:
+            prev_time = self.cumulative_times[-1]
+        self.cumulative_times.append(prev_time + elapsed_time)
+
+    def add_predictions(self, train_predictions, test_predictions, batch_index):
+        train_labels = self.train_label_batches[batch_index]
+        self.train_errors.append(metrics.mean_absolute_error(train_labels, train_predictions))
+        self.train_losses.append(metrics.log_loss(train_labels, train_predictions))
         self.test_losses.append(metrics.log_loss(self.test_labels, test_predictions))
         self.test_errors.append(metrics.mean_absolute_error(self.test_labels, test_predictions))
 
@@ -56,42 +98,28 @@ class Metrics:
         self.train_losses = []
         self.test_losses = []
         self.test_errors = []
+        self.cumulative_times = []
 
     def finish_run(self):
         self.total_test_error.append(self.test_errors)
         self.total_test_loss.append(self.test_losses)
         self.total_train_error.append(self.train_errors)
         self.total_train_loss.append(self.train_losses)
+        self.total_train_times.append(self.cumulative_times)
 
+    def calculate_metric(self, metric_data):
+        all_of_metric = np.array(metric_data)
+        mean_data = np.mean(all_of_metric, axis=0)
+        std_data = np.std(all_of_metric, axis=0)
+        return mean_data, std_data
+    
     def calculate_metrics(self):
         if hasattr(self, 'test_loss_data'):
             return
-
-        all_test_losses = np.array(self.total_test_loss)
-        all_test_errors = np.array(self.total_test_error)
-        all_train_losses = np.array(self.total_train_loss)
-        all_train_errors = np.array(self.total_train_error)
-
-        # calculate averages for each run
-        mean_test_losses = np.mean(all_test_losses, axis=0)
-        mean_test_errors = np.mean(all_test_errors, axis=0)
-        mean_train_losses = np.mean(all_train_losses, axis=0)
-        mean_train_errors = np.mean(all_train_errors, axis=0)
-
-        # calculate standard deviations
-        std_test_losses = np.std(all_test_losses, axis=0)
-        std_test_errors = np.std(all_test_errors, axis=0)
-        std_train_losses = np.std(all_train_losses, axis=0)
-        std_train_errors = np.std(all_train_errors, axis=0)
         
-        self.test_loss_data = (mean_test_losses, std_test_losses)
-        self.test_error_data = (mean_test_errors, std_test_errors)
-        self.train_loss_data = (mean_train_losses, std_train_losses)
-        self.train_error_data = (mean_train_errors, std_train_errors)
+        self.test_loss_data = self.calculate_metric(self.total_test_loss)
+        self.test_error_data = self.calculate_metric(self.total_test_error)
+        self.train_loss_data = self.calculate_metric(self.total_train_loss)
+        self.train_error_data = self.calculate_metric(self.total_train_error)
+        self.train_time_data = self.calculate_metric(self.total_train_times)
 
-
-
-def incremental(train_data, train_labels, test_data, test_labels, num_batches, num_runs, reg_param, num_iterations, annealing=False):
-    data_batches, label_batches = load_incremental_batches(train_data, train_labels)
-
-    incremental_metrics = Metrics(train_labels, test_labels)
